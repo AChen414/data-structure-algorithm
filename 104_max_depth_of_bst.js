8fd8e4a9efe6e353eb49202ddf8283709387d00c
Andrew <achen414@gmail.com>
2021-02-08 03:25:53 -0800

    Solved maxDepthofBST iteratively

--- 104_max_depth_of_bst.js
+++ 104_max_depth_of_bst.js
@@ -4,9 +4,25 @@ Given the root of a binary tree, return its maximum depth.
 A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
 */
 
-function maxDepth(root) {
+function maxDepth(root) { // recursive
     if (root === null) return 0;
     return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+}
+
+function maxDepth(root) { // iterative
+    if (root === null) return 0;
+    const queue = [root];
+    let depth = 0;
+    while (!queue.length) {
+        depth++;
+        const len = queue.length;
+        for (let i = 0; i < len; i++) {
+            if (queue[i].left) queue.push(queue[i].left);
+            if (queue[i].right) queue.push(queue[i].right);
+        }
+        queue.splice(0, len);
+    }
+    return depth;
 }
 
 // Time: O(n) since we visit all nodes
