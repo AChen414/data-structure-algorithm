dd346d217aa1de1bc611af6776d3b5562d695813
Andrew <achen414@gmail.com>
2021-02-11 05:15:01 -0800

    Finished LinkedListCycleII with S/T

--- 142_linked_list_cycle_ii.js
+++ 142_linked_list_cycle_ii.js
@@ -1,9 +1,9 @@
 /*
-Given head, the head of a linked list, determine if the linked list has a cycle in it.
+Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
 
 There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
 
-Return true if there is a cycle in the linked list. Otherwise, return false.
+Notice that you should not modify the linked list.
 
 
 
@@ -11,19 +11,19 @@ Example 1:
 
 
 Input: head = [3,2,0,-4], pos = 1
-Output: true
-Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+Output: tail connects to node index 1
+Explanation: There is a cycle in the linked list, where tail connects to the second node.
 Example 2:
 
 
 Input: head = [1,2], pos = 0
-Output: true
-Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+Output: tail connects to node index 0
+Explanation: There is a cycle in the linked list, where tail connects to the first node.
 Example 3:
 
 
 Input: head = [1], pos = -1
-Output: false
+Output: no cycle
 Explanation: There is no cycle in the linked list.
 */
 
@@ -34,16 +34,21 @@ class LinkedListNode {
     }
 }
 
-
-var hasCycle = function (head) {
+var detectCycle = function (head) {
     let slow = head;
     let fast = head;
     while (fast !== null && fast.next !== null) {
         slow = slow.next;
         fast = fast.next.next;
-        if (fast === slow) return true;
+        if (fast === slow) break;
     }
-    return false;
+    if (fast === null || fast.next === null) return null;
+    let pointer = head;
+    while (pointer !== head) {
+        pointer = pointer.next;
+        head = head.next;
+    }
+    return pointer;
 }
 
 // Time: O(n)
