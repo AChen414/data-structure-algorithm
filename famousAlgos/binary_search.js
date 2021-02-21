652aaedae24f1b041d70867daba6ab04460e43fe
Andrew <achen414@gmail.com>
2021-02-20 20:00:54 -0800

    Finished binarySearch with S/T

---
+++ famousAlgos/binary_search.js
@@ -0,0 +1,18 @@
+function binarySearch(array, target) {
+    let left = 0;
+    let right = array.length - 1;
+    while (left <= right) {
+        let middle = Math.floor(right + left / 2);
+        if (array[middle] === target) {
+            return middle;
+        } else if (target < array[middle]) {
+            right = middle - 1;
+        } else {
+           left = middle + 1;
+        }
+    }
+    return -1;
+}
+
+// Time: O(logn)
+// Space: O(1)
