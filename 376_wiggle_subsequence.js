41014252ef260e8505a92906c72a0abeb08fde45
Andrew <achen414@gmail.com>
2021-03-18 22:43:33 -0700

    Added more optimized solution to wiggleSubsequence

--- 376_wiggle_subsequence.js
+++ 376_wiggle_subsequence.js
@@ -63,3 +63,20 @@ var wiggleMaxLength = function (nums) {
 
 // Time: O(n)
 // Space: O(n)
+
+function wiggleMaxLength(nums) {
+    if (nums.length < 2) return nums.length;
+    let down = 1, up = 1;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > nums[i - 1]) {
+            up = down + 1;
+        }
+        else if (nums[i] < nums[i - 1]) {
+            down = up + 1;
+        }
+    }
+    return Math.max(down, up);
+}
+
+// Time: O(n)
+// Space: O(1)
