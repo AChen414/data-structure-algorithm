87aa5c2dfd1b1b9491cb6c5d5a48e618db585a07
Andrew <achen414@gmail.com>
2021-03-28 22:38:25 -0700

    Attempted pacificAtlanticWaterFlow

--- 417_pacific_atlanta_water_flow.js
+++ 417_pacific_atlanta_water_flow.js
@@ -28,3 +28,45 @@ Return:
 [[0, 4], [1, 3], [1, 4], [2, 2], [3, 0], [3, 1], [4, 0]] (positions with parentheses in above matrix).
 */
 
+var pacificAtlantic = function (matrix) {
+    const result = [];
+    if (matrix.length === 0) return result;
+    const pacific = new Array(matrix.length).fill(new Array(matrix[0].length));
+    const atlantic = new Array(matrix.length).fill(new Array(matrix[0].length));
+    const PQ = [];
+    const AQ = [];
+    for (let i = 0; i < matrix.length; i++) {
+        PQ.push([i, 0]);
+        AQ.push([i, matrix[i].length - 1]);
+        pacific[i][0] = true;
+        atlantic[i][matrix[i].length - 1] = true;
+    }
+    for (let i = 0; i < matrix[0].length; i++) {
+        PQ.push([0, i]);
+        AQ.push([matrix.length - 1, i]);
+        pacific[0][i] = true;
+        atlantic[matrix.length - 1, i] = true;
+    }
+    bfs(matrix, PQ, pacific);
+    bfs(matrix, AQ, atlantic);
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (pacific[i][j] === true && atlantic[i][j] === true) result.push([i, j]);
+        }
+    }
+    return result;
+};
+
+var bfs = function (matrix, queue, visited) {
+    const directions = [[0, 1], [1, 0], [0, -1], [-1, 0]];
+    while (queue.length) {
+        let curr = queue.pop();
+        for (let i = 0; i < directions.length; i++) {
+            const x = curr[0] + directions[i][0];
+            const y = curr[1] + directions[i][1];
+            if (x < 0 || x > matrix.length - 1 || y < 0 || y > matrix[0].length - 1 || matrix[x][y] < matrix[curr[0]][curr[1]] || visited[x][y]) continue;
+            visited[x][y] = true;
+            queue.push([x, y]);
+        }
+    }
+}
