1b7ecbb11d64e47ee666e53df30aa5e527958c3f
Andrew <achen414@gmail.com>
2021-04-02 23:25:30 -0700

    Started 01knapsack

---
+++ famousAlgos/01knapsack.js
@@ -0,0 +1,11 @@
+/*
+Given weights and profits of n items, we are asked to put these items in a knapsack
+that has a capacity c. The goal is to get the max profit from items in the knapsack.
+Each item can only be selected once
+weights = [2, 3, 1, 4]
+profits = [4, 5, 3, 7]
+knapsack = 5
+
+return 10
+*/
+
