06404fc2363b68dce27edb91160ec8471de2e115
Andrew <achen414@gmail.com>
2021-04-03 23:49:06 -0700

    Solved knapsack naively

--- famousAlgos/01knapsack.js
+++ famousAlgos/01knapsack.js
@@ -9,3 +9,21 @@ knapsack = 5
 return 10
 */
 
+// Time: O(2^n)
+// Space: O(n) since it's depth first and we'll only have n recursive calls on the call stack
+
+function knapsack(profits, weights, capacity) {
+    return knapsackRecursion(profits, weights, capacity, 0);
+}
+
+function knapsackRecursion(profits, weights, capacity, currentIndex) {
+    if (capacity <= 0 || currentIndex >= profits.length) return 0;
+
+    let profit1 = 0;
+    if (weights[currentIndex] <= capacity) {
+        profit1 = profits[currentIndex] + knapsackRecursion(profits, weights, capacity - weights[currentIndex], currentIndex + 1);
+    }
+
+    const profit2 = knapsackRecursion(profits, weights, capacity, currentIndex + 1);
+    return Math.max(profit1, profit2);
+}
