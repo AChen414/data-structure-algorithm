8db6acf1d10c0b630ddae171217a658d27b15b21
Andrew <achen414@gmail.com>
2021-04-07 02:11:56 -0700

    Finished stringHalves with S/T

---
+++ 1704_determine_if_string_halves_same.js
@@ -0,0 +1,54 @@
+/*
+You are given a string s of even length. Split this string into two halves of equal lengths, and let a be the first half and b be the second half.
+
+Two strings are alike if they have the same number of vowels ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'). Notice that s contains uppercase and lowercase letters.
+
+Return true if a and b are alike. Otherwise, return false.
+
+
+
+Example 1:
+
+Input: s = "book"
+Output: true
+Explanation: a = "bo" and b = "ok". a has 1 vowel and b has 1 vowel. Therefore, they are alike.
+Example 2:
+
+Input: s = "textbook"
+Output: false
+Explanation: a = "text" and b = "book". a has 1 vowel whereas b has 2. Therefore, they are not alike.
+Notice that the vowel o is counted twice.
+Example 3:
+
+Input: s = "MerryChristmas"
+Output: false
+Example 4:
+
+Input: s = "AbCdEfGh"
+Output: true
+
+
+Constraints:
+
+2 <= s.length <= 1000
+s.length is even.
+s consists of uppercase and lowercase letters.
+*/
+
+var halvesAreAlike = function (s) {
+    let i = 0;
+    let j = s.length - 1;
+    let left = 0;
+    let right = 0;
+    const vowels = { a: true, e: true, i: true, o: true, u: true };
+    while (i < j) {
+        console.log('left ', s[i], ' right ', s[j]);
+        if (vowels[s[i].toLowerCase()]) left++;
+        if (vowels[s[j].toLowerCase()]) right++;
+        i++;
+        j--;
+    }
+    return left === right;
+};
+// Time: O(n)
+// Space: O(1)
