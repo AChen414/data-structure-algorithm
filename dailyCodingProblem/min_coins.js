797ebf55b03717a1354b6afab36ddbc5d75b9f6c
Andrew <achen414@gmail.com>
2021-04-27 23:12:43 -0700

    Started minCoins

---
+++ dailyCodingProblem/min_coins.js
@@ -0,0 +1,11 @@
+/*
+Find the minimum number of coins required to make n cents.
+
+You can use standard American denominations, that is, 1c, 5c, 10c, 25c.
+
+For example, given n = 16, return 3 since we can make it with a 10c, 5c, and 1c coin.
+*/
+
+const minCoins = (n) => {
+    
+}
