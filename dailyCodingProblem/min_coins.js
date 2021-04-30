7c6fcce690b44bdbacfb7a14179fe09abe3632c0
Andrew <achen414@gmail.com>
2021-04-29 23:55:13 -0700

    Finished minCoins

--- dailyCodingProblem/min_coins.js
+++ dailyCodingProblem/min_coins.js
@@ -7,5 +7,15 @@ For example, given n = 16, return 3 since we can make it with a 10c, 5c, and 1c 
 */
 
 const minCoins = (n) => {
-    
+    const coins = [1, 5, 10, 25];
+    const dp = new Array(n + 1).fill(n + 1);
+    dp[0] = 0;
+    for (let i = 1; i < dp.length; i++) {
+        for (let j = 0; j < coins.length; j++) {
+            if (coins[j] <= i) {
+                dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+            }
+        }
+    }
+    return dp[n];
 }
