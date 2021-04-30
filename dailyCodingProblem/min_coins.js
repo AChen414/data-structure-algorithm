e7b3970c7b6c392a8b056e9f5d0e48358dfc767e
Andrew <achen414@gmail.com>
2021-04-29 23:56:59 -0700

    Thought of brute force solution

--- dailyCodingProblem/min_coins.js
+++ dailyCodingProblem/min_coins.js
@@ -4,6 +4,8 @@ Find the minimum number of coins required to make n cents.
 You can use standard American denominations, that is, 1c, 5c, 10c, 25c.
 
 For example, given n = 16, return 3 since we can make it with a 10c, 5c, and 1c coin.
+
+Brute force solution would be to go through each coin and subtract it from amount til we hit 0
 */
 
 const minCoins = (n) => {
