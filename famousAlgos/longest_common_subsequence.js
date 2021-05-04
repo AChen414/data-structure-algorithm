d3c6d1e60e1f51a23ed4a5d3670aef3eb174f2d2
Andrew <achen414@gmail.com>
2021-05-03 23:38:21 -0700

    Added notes to longestCommonSubsequence

--- famousAlgos/longest_common_subsequence.js
+++ famousAlgos/longest_common_subsequence.js
@@ -1,9 +1,12 @@
 const longestCommonSubsequence = (string1, string2) => {
+    // Initialize dp array for string comparison. Array at position is answer to LCS
+    // if the strings were to end at that index
     const dp = [];
     for (let i = 0; i < string2.length; i++) {
         dp[i] = new Array(string1.length + 1).fill(0);
     }
-
+    
+    // Go through each combination of strings to find answer at dp array index
     for (let i = 1; i <= string1.length; i++) {
         for (let j = 1; j <= string2.length; j++) {
             if (string1[i - 1] !== string2[j - 1]) {
